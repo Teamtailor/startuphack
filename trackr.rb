7d775c91e6a3298dcf42a497bf32bbfd884a5f36
Richard Johansson <richard@newsdesk.se>
2012-05-12 11:13:42 +0200

    Fix Redis Heroku config variable

--- trackr.rb
+++ trackr.rb
@@ -5,7 +5,7 @@ class Trackr
   # attr_accessor :redis, :site, :uid
   def initialize(env = ENV['RACK_ENV'])
     if env == "production"
-      uri = URI.parse(ENV["@redisTOGO_URL"])
+      uri = URI.parse(ENV["REDISTOGO_URL"])
       @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     else
       @redis = Redis.new
