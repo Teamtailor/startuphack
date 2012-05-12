22709ff5eb854854c0cb6157a5990c012eb3a9ae
Richard Johansson <richard@newsdesk.se>
2012-05-12 11:39:51 +0200

    Add tracking to this app!

--- app.rb
+++ app.rb
@@ -7,23 +7,48 @@ class App < Sinatra::Base
   TRACKR = Trackr.new
 
   aget '/' do
+    env = ENV['RACK_ENV']
     sites = TRACKR.sites
     sites_str = sites.map do |site|
       "<li><a href='/stats/#{site}'>#{site.to_s}</a></li>"
     end
 
+    local_domain = env == "production" ? "startuphack.herokuapp.com" : "localhost:3000"
+
     body <<-BODY
-      <h1>Trackr</h1>
-      <p>
-        To track a user: /track/mynewsdesk_com/123
-      </p>
-      <p>
-        To get stats: /stats/mynewsdesk_com
-      </p>
-      <h2>Current sites</h2>
-      <ul>
-        #{sites_str.join}
-      </ul>
+      <html>
+        <head>
+          <script type="text/javascript">
+            var _trackr_config_variables={domain:"#{local_domain}"};
+            (function(){
+              function loadTrackr() {
+                window._sf_endpt=(new Date()).getTime();
+                var e = document.createElement('script');
+                e.setAttribute('language', 'javascript');
+                e.setAttribute('type', 'text/javascript');
+                e.setAttribute('src',"//#{local_domain}/trackr#{"-local" unless env == "production"}.js");
+                document.body.appendChild(e);
+              }
+              var oldonload = window.onload;
+              window.onload = (typeof window.onload != 'function') ?
+                 loadTrackr : function() { oldonload(); loadTrackr(); };
+            })();
+          </script>
+        </head>
+        <body>
+          <h1>Trackr</h1>
+          <p>
+            To track a user: /track/mynewsdesk_com/123
+          </p>
+          <p>
+            To get stats: /stats/mynewsdesk_com
+          </p>
+          <h2>Current sites</h2>
+          <ul>
+            #{sites_str.join}
+          </ul>
+        </body>
+      </html>
     BODY
   end
 
