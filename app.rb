0e0c92944609956473c768f8ecc6d7af68206869
Richard Johansson <richard@newsdesk.se>
2012-05-12 10:50:24 +0200

    Extract Trackr logic into new class

--- app.rb
+++ app.rb
@@ -1,19 +1,13 @@
 require 'sinatra/async'
-require 'uri'
-require 'redis'
+require_relative 'trackr'
 
 class App < Sinatra::Base
   register Sinatra::Async
 
-  if ENV['RACK_ENV'] == "production"
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  else
-    REDIS = Redis.new
-  end
+  TRACKR = Trackr.new
 
   aget '/' do
-    sites = REDIS.keys "*"
+    sites = TRACKR.sites
     sites_str = sites.map do |site|
       "<li><a href='/stats/#{site}'>#{site.to_s}</a></li>"
     end
@@ -34,30 +28,25 @@ class App < Sinatra::Base
   end
 
   aget '/track/:site/:uid' do
-    time = Time.now
-    site = params[:site].gsub(".", "_")
+    site = params[:site]
     uid = params[:uid]
 
-    REDIS.zadd site, time.to_i, uid
-    body "stored #{time.to_i} for user #{uid} in site #{site}"
+    site, uid, value = TRACKR.track!(site, uid)
+
+    body "stored #{value} for user #{uid} in site #{site}"
   end
 
   aget '/stats/:site' do
     site = params[:site]
-    min_score = Time.now.to_i - (60 * 5) # 5 minutes ago
-    max_score = Time.now.to_i
-
-    total_visitors = REDIS.zcard(site)
-    recent_visitors = REDIS.zrangebyscore(site, min_score, max_score)
 
     body <<-BODY
       <h1>#{site}</h1>
       <ul>
         <li>
-          Totals visitors (since last Redis restart): #{total_visitors}
+          Totals visitors (since last Redis restart): #{TRACKR.total_visitors(site)}
         </li>
         <li>
-          Visitors last 5 minutes: #{recent_visitors}
+          Visitors last 5 minutes: #{TRACKR.recent_visitors(site)}
         </li>
       </ul>
     BODY
---
+++ trackr.rb
@@ -0,0 +1,42 @@
+require 'uri'
+require 'redis'
+
+class Trackr
+  # attr_accessor :redis, :site, :uid
+  def initialize(env = ENV['RACK_ENV'])
+    if env == "production"
+      uri = URI.parse(ENV["@redisTOGO_URL"])
+      @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    else
+      @redis = Redis.new
+    end
+  end
+
+  def sites
+    @redis.keys "*"
+  end
+
+  def track!(site, uid)
+    value = Time.now.to_i
+    sanitize!(site)
+
+    @redis.zadd site, value, uid
+    [site, uid, value]
+  end
+
+  def total_visitors(site)
+    @redis.zcard(site)
+  end
+
+  def recent_visitors(site)
+    min_score = Time.now.to_i - (60 * 5) # 5 minutes ago
+    max_score = Time.now.to_i
+
+    @redis.zrangebyscore(site, min_score, max_score)
+  end
+
+  private
+  def sanitize!(site)
+    site.gsub!(".", "_")
+  end
+end
